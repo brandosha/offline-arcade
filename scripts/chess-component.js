89fe3cd7a83ed9c1bbd6ef0df868f6e309d405e5
brandosha <brandon.lantz@icloud.com>
2022-03-09 16:37:14 +0400

    Allow moving through history after the game is over

--- scripts/chess-component.js
+++ scripts/chess-component.js
@@ -7,7 +7,8 @@ Vue.component("chess", {
     if (pgn) {
       chess.load_pgn(pgn)
     }
-    
+
+    const moveHistory = chess.history()
 
     return {
       chess,
@@ -20,12 +21,18 @@ Vue.component("chess", {
       
       moves: {},
       movingPiece: null,
+
+      gameOver: false,
+      history: {
+        index: moveHistory.length - 1,
+        moves: moveHistory
+      }
     }
   },
 
   methods: {
     squareClicked(row, col) {
-      if (this.chess.game_over()) {
+      if (this.gameOver) {
         return
       }
 
@@ -53,9 +60,13 @@ Vue.component("chess", {
       const to = this.squareName(row, col)
 
       const promotion = "q" // TODO: Allow player to select promotion piece
-      this.chess.move({ from, to, promotion })
+      const move = this.chess.move({ from, to, promotion })
+
+      this.history.moves.push(move.san)
+      this.history.index = this.history.moves.length - 1
 
       this.updateBoard()
+      this.updateGameState()
     },
     updateBoard() {
       const { chess } = this
@@ -63,21 +74,41 @@ Vue.component("chess", {
       this.board = chess.board()
       this.movingPiece = null
 
-      this.pgn = chess.pgn()
-      this.fen = chess.fen()
-
       const moveObj = {}
       const moves = chess.moves({ verbose: true })
       moves.forEach(move => {
         moveObj[move.from + move.to] = move
       })
       this.moves = moveObj
+    },
+    updateGameState() {
+      const { chess } = this
+
+      this.pgn = chess.pgn()
+      this.fen = chess.fen()
 
       localStorage.setItem("chess-pgn", this.pgn)
     },
     reset() {
+      this.gameOver = false
       this.chess.reset()
       this.updateBoard()
+
+      localStorage.removeItem("chess-pgn")
+    },
+    historyBack() {
+      if (this.history.index >= 0) {
+        this.history.index--
+        this.chess.undo()
+        this.updateBoard()
+      }
+    },
+    historyForward() {
+      if (this.history.index < this.history.moves.length - 1) {
+        this.history.index++
+        this.chess.move(this.history.moves[this.history.index])
+        this.updateBoard()
+      }
     }
   },
   computed: {
@@ -87,12 +118,16 @@ Vue.component("chess", {
       const turn = this.chess.turn() == "w" ? "White" : "Black"
 
       if (this.chess.in_checkmate()) {
+        this.gameOver = true
         return turn + " wins"
       } else if (this.chess.in_stalemate()) {
+        this.gameOver = true
         return "Stalemate"
       } else if (this.chess.in_threefold_repetition()) {
+        this.gameOver = true
         return "Threefold repetition"
       } else if (this.chess.in_draw()) {
+        this.gameOver = true
         return "Draw"
       }
 
